2fd646b0ba994df323dd27a9b270c7b1a918367c
manikumar-xts <manikumar@xtsservices.com>
2026-02-06 14:28:06 +0530

    delete api for estimations

--- src/routes/invoiceRoutes.ts
+++ src/routes/invoiceRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { listInvoices, createInvoiceHandler, downloadInvoice } from '../controllers/invoiceController';
+import { listInvoices, createInvoiceHandler, downloadInvoice, deleteInvoice } from '../controllers/invoiceController';
 
 const router = Router();
 
 router.get('/', listInvoices);
 router.post('/', createInvoiceHandler);
 router.get('/:id/download', downloadInvoice);
+router.delete('/:id', deleteInvoice);
 
 export default router;
